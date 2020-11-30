3bacb2ed615556e639f073174867e8f7e2b800a8
sgershuny <72019925+sgershuny@users.noreply.github.com>
2020-11-30 18:24:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-//window.location.replace("http://www.w3schools.com");
-console.log("found DA page")
-
 let webArray = ['https://www.bbc.co.uk/sport/football', 'https://www.bbc.co.uk/news', 'https://www.bbc.co.uk/weather'];
 
 let website = webArray[Math.floor(Math.random() * webArray.length)];
-
-window.location.replace(website);
+console.log(website)
+//window.location.replace(website);
