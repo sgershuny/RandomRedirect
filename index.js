3f4f64addc280d142508e1b5fdb10998d2a9474a
sgershuny <72019925+sgershuny@users.noreply.github.com>
2020-12-02 15:17:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const Datastore = require('nedb');
+
+const database = new Datastore('database.db');
+
+database.loadDatabase();
+
 let webArray = ['https://www.bbc.co.uk/sport/football', 'https://www.bbc.co.uk/news', 'https://www.bbc.co.uk/weather'];
 
 let website = webArray[Math.floor(Math.random() * webArray.length)];
-console.log(website)
+
+database.insert({webpageVisit : website})
 window.location.replace(website);
